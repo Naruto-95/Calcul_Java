e89cabb3729e55c5924b57bc83d96876d52a4c43
iliy <gromov218974@mail.ru>
2022-04-03 15:26:38 +0400

    Прописал айди кнопок в 2-ух массивах

--- app/src/main/java/com/example/calcul_java/MainActivity.java
+++ app/src/main/java/com/example/calcul_java/MainActivity.java
@@ -10,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//id кнопок калькулятора. сначала цифры
+        int numberID[] = new int[]{
+                R.id.zero,
+                R.id.one,
+                R.id.two,
+                R.id.three,
+                R.id.forr,
+                R.id.five,
+                R.id.six,
+                R.id.saven,
+                R.id.eight,
+                R.id.nine
+        };
+        //id кнопок с действиями
+        int numberActions [] = new int[]{
+                R.id.plus,
+                R.id.minus,
+                R.id.exactly,
+                R.id.multip,
+                R.id.devision
+
+        };
     }
 }
